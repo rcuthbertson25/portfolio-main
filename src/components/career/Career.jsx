5daf82b792e69c395fdc121fcb33c73695595945
KingKodoZ <ryancuth25@yahoo.com>
2025-08-13 23:56:20 -0400

    Updated image ref

--- src/components/career/Career.jsx
+++ src/components/career/Career.jsx
@@ -5,17 +5,17 @@ const Career = () => {
     <div>
       <div className="career">
         <img
-          src="/public/Micronetbd_Logo.webp"
+          src="./Micronetbd_Logo.webp"
           alt="Micronet Logo"
           className="logo"
         />
         <div className="partners">
           <img
-            src="/public/salesforce-partner-logo.png"
+            src="./salesforce-partner-logo.png"
             alt="Salesforce Partner Logo"
             className="salesforce"
           />
-          <img src="/public/litify.svg" alt="Litify Logo" className="litify" />
+          <img src="./litify.svg" alt="Litify Logo" className="litify" />
         </div>
       </div>
       <div className="job-title">
@@ -26,22 +26,22 @@ const Career = () => {
         <div className="card">
           <h3>User Management</h3>
           <p>Managed user accounts, roles, and permission sets to ensure secure and efficient access to Salesforce data.</p>   
-          <img src="/public/user-management.webp" alt="User Management Icon" className="icon" />
+          <img src="./user-management.webp" alt="User Management Icon" className="icon" />
         </div>
         <div className="card">
           <h3>Data Operations</h3>
           <p>Managed data imports, exports, and mass updates; supported data quality through regular queries, validations, and workflow automation.</p>
-          <img src="/public/data.webp" alt="Data Operations Icon" className="icon" />
+          <img src="./data.webp" alt="Data Operations Icon" className="icon" />
         </div>
         <div className="card">
           <h3>Automation</h3>
           <p>Built and maintained workflows, process builders, and flows to automate routine tasks and improve team productivity.</p>
-          <img src="/public/flow.webp" alt="Automation Icon" className="icon" />
+          <img src="./flow.webp" alt="Automation Icon" className="icon" />
         </div>
         <div className="card">
           <h3>Reporting</h3>
           <p>Created custom reports and dashboards to support business decision-making and track key performance indicators (KPIs).</p>
-          <img src="/public/report.webp" alt="Reporting Icon" className="icon" />
+          <img src="./report.webp" alt="Reporting Icon" className="icon" />
         </div>
       </div>
       <div className="skill-section">
